58e091dbf8419f3bbd2906f020992b43568cd429
Shuff-Alv <alvarocastilloperez@gmail.com>
2022-12-30 13:55:38 +0100

    Add files

---
+++ CPU.srcs/sources_1/new/sequencer.vhd
@@ -0,0 +1,39 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 30.12.2022 13:48:55
+-- Design Name: 
+-- Module Name: sequencer - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+ENTITY sequencer IS
+    PORT (prueba : IN STD_LOGIC);
+END sequencer;
+
+ARCHITECTURE Behavioral OF sequencer IS
+
+BEGIN
+END Behavioral;
