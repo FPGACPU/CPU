e27233f8d5faf5dc86516569cd0fec03c1006ad7
JuanLuqueGaze <juanluquerobotic@gmail.com>
2022-12-30 12:16:05 +0100

    SYNCREGISTER 1.1

--- CPU.srcs/sources_1/new/syncregister.vhd
+++ CPU.srcs/sources_1/new/syncregister.vhd
@@ -5,42 +5,31 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 ENTITY syncregister IS
     GENERIC (
-
-        N : INTEGER := 12
-
-    );
+        N : INTEGER := 12);
     PORT (
         clk : IN STD_LOGIC;
         ld : IN STD_LOGIC;
         clr : IN STD_LOGIC;
         data : IN UNSIGNED(0 TO (N - 1));
-        ex : OUT UNSIGNED(0 TO (N - 1))
-
-    );
+        output : OUT UNSIGNED(0 TO (N - 1)));
 END syncregister;
 
 ARCHITECTURE Behavioral OF syncregister IS
 
-    SIGNAL pex : UNSIGNED(0 TO (N - 1));
-
+    SIGNAL output_reg : UNSIGNED(0 TO (N - 1));
 BEGIN
 
     sync : PROCESS (clk, ld, clr, data)
     BEGIN
-
         IF CLR = '1' THEN
-            pex(0 TO (N - 1)) <= (OTHERS => '0');
-
+            output_reg(0 TO (N - 1)) <= (OTHERS => '0');
         ELSE
             IF (rising_edge(clk) AND ld = '1') THEN
-
-                pex <= data;
-
+                output_reg <= data;
             END IF;
         END IF;
-
     END PROCESS;
 
-    ex <= pex;
+    output <= output_reg;
 
 END Behavioral;
