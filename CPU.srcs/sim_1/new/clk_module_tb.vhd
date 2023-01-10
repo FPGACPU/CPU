d243f34efbdef24101d0728e1035fc3a5e567b26
marcasmar1 <castellmartinmarco1@gmail.com>
2023-01-10 16:07:09 +0100

    Added clk_module

--- CPU.srcs/sim_1/new/clk_module_tb.vhd
+++ CPU.srcs/sim_1/new/clk_module_tb.vhd
@@ -7,20 +7,26 @@ ENTITY clk_module_tb IS
 END clk_module_tb;
 
 ARCHITECTURE Behavioral OF clk_module_tb IS
-    COMPONENT clk_divider
-        GENERIC (N : INTEGER := 4);
+
+    COMPONENT clk_divider IS
+        GENERIC (Divider : INTEGER := 4); --Divider 2^(N-1)
         PORT (
             clk_in : IN STD_LOGIC;
+            trace : IN STD_LOGIC; --Synchronous activation
+            pulse : IN STD_LOGIC;
             clk_out : OUT STD_LOGIC);
     END COMPONENT;
 
-    SIGNAL clk_in, clk_out : STD_LOGIC := '0';
+    SIGNAL clk_in, trace, pulse : STD_LOGIC := '0';
+    SIGNAL clk_out : STD_LOGIC;
     CONSTANT clk_period : TIME := 10ns;
 
 BEGIN
     DUT : clk_divider
     PORT MAP(
         clk_in => clk_in,
+        trace => trace,
+        pulse => pulse,
         clk_out => clk_out);
 
     clk : PROCESS
@@ -30,4 +36,21 @@ BEGIN
         clk_in <= '1';
         WAIT FOR clk_period/2;
     END PROCESS;
+
+    clk_module_sim : PROCESS
+    BEGIN
+        WAIT FOR 170ns;
+        pulse <= '1';
+        WAIT FOR 80ns;
+        pulse <= '0';
+        trace <= '1';
+        WAIT FOR 70ns;
+        pulse <= '1';
+        WAIT FOR 6ns;
+        pulse <= '0';
+        trace <= '0';
+        WAIT FOR 80ns;
+
+    END PROCESS;
+
 END Behavioral;
--- CPU.srcs/sources_1/new/clk_module.vhd
+++ CPU.srcs/sources_1/new/clk_module.vhd
@@ -3,23 +3,31 @@ USE IEEE.STD_LOGIC_1164.ALL;
 USE IEEE.NUMERIC_STD.ALL;
 
 ENTITY clk_divider IS
-    GENERIC (N : INTEGER := 4); --Divider 2^(N-1)
+    GENERIC (Divider : INTEGER := 4); --Divider 2^(N-1)
     PORT (
         clk_in : IN STD_LOGIC;
+        trace : IN STD_LOGIC; --Synchronous activation
+        pulse : IN STD_LOGIC;
         clk_out : OUT STD_LOGIC);
 END clk_divider;
 
 ARCHITECTURE Behavioral OF clk_divider IS
-    SIGNAL count, new_count : unsigned(0 TO N - 1) := to_unsigned(0, N);
+    SIGNAL count : unsigned(0 TO Divider - 1) := to_unsigned(0, Divider);
 
 BEGIN
-    new_count <= count + 1;
-    clk_out <= STD_LOGIC(count(0));
+    clk_out <= STD_LOGIC(count(0)) OR (trace AND pulse);
 
     sync : PROCESS (clk_in)
+        VARIABLE trace_start : BOOLEAN;
     BEGIN
-        IF falling_edge(clk_in) THEN
-            count <= new_count;
+        IF (trace = '1' AND (count = to_unsigned(0, Divider))) THEN
+            trace_start := True;
+        ELSE
+            trace_start := False;
+        END IF;
+
+        IF falling_edge(clk_in) AND NOT trace_start THEN
+            count <= count + 1;
         END IF;
     END PROCESS;
 
