ca998d62e2d7f2d1ff0283692c531ee3d550a384
JuanLuqueGaze <juanluquerobotic@gmail.com>
2023-02-02 19:05:50 +0100

    changes...

---
+++ CPU.srcs/sim_1/new/cpu_tb.vhd
@@ -0,0 +1,63 @@
+LIBRARY IEEE;
+USE IEEE.STD_LOGIC_1164.ALL;
+USE IEEE.NUMERIC_STD.ALL;
+USE STD.ENV.FINISH;
+
+ENTITY cpu_tb IS
+END cpu_tb;
+
+ARCHITECTURE Behavioral OF cpu_tb IS
+    COMPONENT cpu
+        PORT (
+            clk : IN STD_LOGIC;
+            lec, esc : OUT STD_LOGIC;
+            data_bus : INOUT STD_LOGIC_VECTOR(0 TO 11);
+            addr_bus : OUT STD_LOGIC_VECTOR(0 TO 8);
+            ac_debug : OUT UNSIGNED(0 TO 11);
+            reset : IN STD_LOGIC
+        );
+    END COMPONENT;
+
+    CONSTANT clk_period : TIME := 10 ns;
+    SIGNAL clk : STD_LOGIC := '1';
+    SIGNAL lec : STD_LOGIC;
+    SIGNAL esc : STD_LOGIC;
+    SIGNAL data_bus : STD_LOGIC_VECTOR(0 TO 11) := "000000000000";
+    SIGNAL addr_bus : STD_LOGIC_VECTOR(0 TO 8);
+    SIGNAL ac_debug : UNSIGNED(0 TO 11);
+    SIGNAL reset : STD_LOGIC := '0';
+
+BEGIN
+    DUT : cpu
+    PORT MAP(
+        clk => clk,
+        lec => lec,
+        esc => esc,
+        data_bus => data_bus,
+        addr_bus => addr_bus,
+        ac_debug => ac_debug,
+        reset => reset);
+
+    clk_process : PROCESS
+    BEGIN
+        clk <= '0';
+        WAIT FOR clk_period/2;
+        clk <= '1';
+        WAIT FOR clk_period/2;
+    END PROCESS;
+
+    simcpu : PROCESS
+    BEGIN
+        data_bus <= "010000000100";
+        WAIT FOR 40 ns;
+        data_bus <= "000000000010";
+        WAIT FOR 50 ns;
+        reset <= '1';
+        WAIT FOR 60 ns;
+        data_bus <= "111000000000";
+        WAIT FOR 70 ns;
+
+        FINISH;
+    END PROCESS;
+
+END Behavioral;
